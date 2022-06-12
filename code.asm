e58064534729fa026ec405945b2a05ca3e23fed1
Abdullah Sattar <103134208+abdullahsattaar@users.noreply.github.com>
2022-06-12 07:59:42 +0500

    Add files via upload

---
+++ code.asm
@@ -0,0 +1,23 @@
+[org 0x0100]
+
+mov dx,76
+mov bx,47
+sub dx,bx
+mov bx,29
+sub dx,bx
+mov bx,18
+sub dx,bx
+mov bx,11
+sub dx,bx
+mov bx,7
+sub dx,bx
+mov bx,4
+sub dx,bx
+mov bx,3
+sub dx,bx
+mov bx,1
+sub dx,bx
+mov bx,2
+sub dx,bx
+mov dx, 0x4c00
+int 0x21
